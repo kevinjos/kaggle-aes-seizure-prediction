237b02622b0503746efd4f2fa11ede82da1f32cc
kjs <kjs@llama>
2014-09-07 12:32:20 -0700

    EEG Cine for raw and fft data

--- explore.py
+++ explore.py
@@ -13,10 +13,12 @@ import numpy as np
 import os
 import sys
 import fftw3
-import decs
+from itertools import islice
 from collections import deque
 from PyQt4 import QtGui, QtCore
 import pyqtgraph as pg
+
+import objgraph
 
 class FileHandler(object):
   DATA_DIR = '/home/kjs/repos/kaggle-aes-seizure-prediction/data/'
@@ -25,87 +27,96 @@ class FileHandler(object):
     print "Configure the data directory to match local directory structure"
 
   def __init__(self):
-    self.data = self.get_data(self.DATA_DIR + 'Dog_1/Dog_1_preictal_segment_0001.mat')
+    self.file_in = 'Dog_1/Dog_1_preictal_segment_0001.mat'
 
-  def get_data(self, matfile):
-    with open(matfile) as f:
+  def get_data(self):
+    self.file_in = "_".join(self.file_in.split("_")[0:2]) + "/" + self.file_in
+    with open(self.DATA_DIR + self.file_in) as f:
       mat = scipy.io.loadmat(f)
-      if mat.keys()[2].find('segment') > -1:
-        data = mat[mat.keys()[2]]
-      else:
-        print "Key index is off for this data file. Write a better method to find the correct key for the data."
-    return data[0,0]
-
-  def get_channel(self, channels=[]):
-    try:
-      channel_data = self.data[0][channels[0], ]
-    except IndexError:
-      channel_data = None
-      print "Channel does not exist, choose a channel betwee 0 and %s" % (data[0].shape[0] - 1)
-    return channel_data
+      keys = mat.keys()
+      i = [mat.keys().index(key) for key in mat.keys() if key.find('segment') > 0][0]
+      data = mat[mat.keys()[i]]
+    self.data = data[0,0]
+    self.data_length_sec = self.data[1][0]
+    self.frequency = self.data[2][0]
+    self.electrode_names = self.data[3][0]
+    self.sequence_num = self.data[4]
 
 class Cine(object):
-  def __init__(self):
+  def __init__(self, FileHandler):
+    self.filehandler = FileHandler
     self.app = QtGui.QApplication(sys.argv)
     self.widget = QtGui.QWidget()
     self.start_btn = QtGui.QPushButton('Start')
     self.stop_btn = QtGui.QPushButton('Stop')
-    self.rawplot = pg.PlotWidget()
-    self.fftplot = pg.PlotWidget()
-    self.rawplot.setRange(yRange=(500, -500))
-    self.fftplot.setRange(yRange=(0, 90))
+    self.file_in = QtGui.QInputDialog()
     self.layout = QtGui.QGridLayout()
     self.layout.addWidget(self.start_btn, 0, 0)
     self.layout.addWidget(self.stop_btn, 0, 1)
-    self.layout.addWidget(self.rawplot, 1, 0, 2, 4)
-    self.layout.addWidget(self.fftplot, 5, 0, 2, 4)
     self.widget.setLayout(self.layout)
     self.start_btn.clicked.connect(self.start)
     self.stop_btn.clicked.connect(self.stop)
     self.widget.show()
-    self.x_time = deque([0], 1024)
-    self.y_val = deque([0], 1024)
+
+  def cons_plots(self, num_channels):
+    for i in range(num_channels):
+      i = str(i)
+      setattr(self, 'rawplot_' + i, pg.PlotWidget())
+      getattr(self, 'rawplot_' + i).setRange(yRange=(1000, -1000))
+      setattr(self, 'fftplot_' + i, pg.PlotWidget())
+      getattr(self, 'fftplot_' + i).setRange(yRange=(0, 170))
+      self.layout.addWidget(getattr(self, 'rawplot_' + i))
+      self.layout.addWidget(getattr(self, 'fftplot_' + i))
 
   def start(self):
-    pass
+    self.filehandler.file_in = str(self.file_in.getText(self.widget, "", "what film?")[0])
+    self.filehandler.get_data()
+    num_channels = self.filehandler.data[0].shape[0]
+    num_samples = self.filehandler.data[0].shape[1]
+    self.make_iterators(num_channels)
+    self.plot(num_channels, num_samples)
 
   def stop(self):
+    sys.exit()
     self.x_time.clear()
     self.y_val.clear()
 
-  def do_fft(self, fft_mode):
-    fft_size = 512
-    bins = [i for i in range(fft_size/2)]
-    while True:
-      val, time_s = yield
-      self.x_time.append(time_s)
-      self.y_val.append(val)
+  def make_iterators(self, num_channels):
+    for i in range(num_channels):
+      setattr(self, 'rawchan_' + str(i), self.filehandler.data[0][i].flat)
+      
+  def do_fft(self, inputa):
+    outputa = np.zeros(self.fft_size, dtype=complex)
+    fft_plan = fftw3.Plan(inputa, outputa, direction='forward', flags=['estimate'])
+    fft_plan.execute()
+    return (np.log10(np.abs(outputa)) * 20)[:self.fft_size/2]
 
-  @decs.coroutine
-  def plot(self, fft_mode):
-    while True:
-      val, time_s = yield
-      self.x_time.append(time_s)
-      self.y_val.append(val)
-      if seq_num % 32 == 0 and len(self.raw_y) >= fft_size:
-        y_val_list = list(self.raw_y)
-        inputa = np.array(raw_y_list[-fft_size:], dtype=complex)
-        #hann_window = np.hanning(fft_size)
-        #inputa = inputa * hann_window
-        #inputa = inputa * flattop_window
-        outputa = np.zeros(fft_size, dtype=complex)
-        if fft_mode == '1':
-          fft = fftw3.Plan(inputa, outputa, direction='forward', flags=['estimate'])
-          fft.execute()
-          outputa = (np.log10(np.abs(outputa)) * 20)[:fft_size/2]
-          self.fftplot.plot(bins, outputa, clear=True)
-        self.rawplot.plot(self.raw_x, self.raw_y, clear=True)
-        pg.QtGui.QApplication.processEvents()
+  def plot(self, num_channels, num_samples):
+    #initialize parameters, graphs, and collections for plotting
+    self.cons_plots(num_channels)
+
+    self.fft_size, fft_stop = 5000, 10000
+    fft_start = fft_stop - self.fft_size
+
+    bins = [i for i in xrange(self.fft_size/2)]
+    x_time = deque([0], fft_stop)
+    y_val = [deque([0], fft_stop) for i in range(num_channels)]
+    #iterate over the data and make plots
+    for time_s in xrange(num_samples):
+      x_time.append(time_s)
+      for i in range(num_channels):
+        y_val[i].append(getattr(self, 'rawchan_' + str(i)).next())
+      if time_s % 5000 == 0 and len(y_val[0]) >= fft_stop:
+        for i in range(num_channels):
+          outputa = self.do_fft(np.array(list(islice(y_val[i], fft_start, fft_stop)), dtype=complex))
+          #getattr(self, 'fftplot_' + str(i)).plot(bins[:200], outputa[0:200], clear=True)
+          #getattr(self, 'rawplot_' + str(i)).plot([x/5000.0 for x in x_time], y_val[i], clear=True)
+        #self.app.processEvents()
+        print objgraph.show_most_common_types()
 
 if __name__ == '__main__':
   fh = FileHandler()
-  channel_data = fh.get_channel(channels=[0])
-  cine = Cine()
+  cine = Cine(fh)
   cine.app.exec_()
-
+    
 
