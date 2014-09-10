b35628ac1fc4daf87e45e4caa32279a53f5feae1
kjs <kjs@llama>
2014-09-10 08:57:02 -0700

    move io to root process in feature extractor

--- feature_extractor.py
+++ feature_extractor.py
@@ -14,9 +14,10 @@ class FeatureExtractor(object):
   def __init__(self, data):
     self.features = {}
     self.data = data
-  def set_features(self):
+  def set_features(self,filen):
     #self.set_hurst()
     self.set_foo()
+    self.set_filen(filen)
   def set_hurst(self):
     for i in range(self.data.shape[0]):
       name = 'hurst_e' + str(i)
@@ -26,12 +27,16 @@ class FeatureExtractor(object):
       name = 'foo'
       value = str(self.data[0][0])
       self.features[name] = value
-  def record_features(self, file_in):
-    feature_dir = '/home/kjs/repos/kaggle-aes-seizure-prediction/feature/'
-    feature_file = file_in.split('.')[0] + '_features.csv'
-    with open(feature_dir + feature_file, 'w') as f:
-      for feature in self.features.keys():
-        f.write(",".join([feature, self.features[feature]]))
+  def set_filen(self, filen):
+      self.features['filen'] = filen
+
+def record_features(features):
+  feature_dir = '/home/kjs/repos/kaggle-aes-seizure-prediction/feature/'
+  feature_file = features['filen'].split('.')[0] + '_features.csv'
+  with open(feature_dir + feature_file, 'w') as f:
+    for feature in features.keys():
+      if feature != 'filen':
+        f.write(",".join([feature, features[feature]]))
 
 def main():
   fh = file_handler.FileHandler()
@@ -42,29 +47,33 @@ def main():
   size = comm.Get_size()
 
   train_file_buffer = np.array([''.zfill(37)])
-  truth_buffer = np.array([0, ])
-  human_train_array = np.array([np.array([f]) for f in fh.seg_train_files['Patient']])
-  dog_train_array = np.array([np.array([f]) for f in fh.seg_train_files['Dog']])
-  all_train_array = np.array([np.array([f]) for f in fh.all_train_files])
+  train_files = np.array([np.array([f]) for f in fh.all_train_files])
+  train_data_buffer = np.array([np.zeros(4500000) for i in range(24)])
 
   if rank == 0:
     i = 0
-    for f in all_train_array:
+    for f in train_files:
       i += 1
       proc = (i % (size - 1)) + 1
-      comm.Send([f, MPI.SIGNED_CHAR], dest=proc)
-      comm.Recv([truth_buffer, MPI.INT], source=MPI.ANY_SOURCE)
-  else:    
-    for i in range(len(human_train_array)):
-      comm.Recv([train_file_buffer, MPI.SIGNED_CHAR], source=0)
-      fh.file_in = train_file_buffer[0]
+
+      fh.file_in = f[0]
       fh.set_data()
 
-      fe = FeatureExtractor(fh.data[0])
-      fe.set_features()
-      fe.record_features(train_file_buffer[0])
-      truth_buffer[0] = 1
-      comm.Send([truth_buffer, MPI.INT], dest=0)
+      data = fh.data[0]
+
+      comm.Send([f, MPI.SIGNED_CHAR], dest=proc)
+      comm.Send([data, MPI.SIGNED_INT], dest=proc)
+      features = comm.recv(source=MPI.ANY_SOURCE)
+      record_features(features)
+  else:    
+    for i in range(len(train_files)):
+      comm.Recv([train_file_buffer, MPI.SIGNED_CHAR], source=0)
+      comm.Recv([train_data_buffer, MPI.SIGNED_INT], source=0)
+
+      fe = FeatureExtractor(train_data_buffer)
+      fe.set_features(train_file_buffer[0])
+
+      comm.send(fe.features, dest=0)
 
 if __name__ == '__main__':
-  main()
+  MAIN()
